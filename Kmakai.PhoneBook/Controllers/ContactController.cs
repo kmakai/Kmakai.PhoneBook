cc194292b0fb38adf36e18891f5ecf282a2ecf07
kmakai <makaikwag@gmail.com>
2023-07-07 19:44:34 -0400

    model, dbcontext, and controller created

---
+++ Kmakai.PhoneBook/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using Kmakai.PhoneBook.Data;
+using Kmakai.PhoneBook.Models;
+
+namespace Kmakai.PhoneBook.Controllers;
+
+public class ContactController
+{
+    public static void AddContact(Contact contact)
+    {
+        using var db = new AppDbContext();
+        db.Contacts.Add(contact);
+        db.SaveChanges();
+    }
+
+    public static Contact GetContact(int id)
+    {
+        using var db = new AppDbContext();
+        var contact = db.Contacts.SingleOrDefault(x => x.Id == id);      
+        return contact;
+    }
+
+    public static List<Contact> GetContacts()
+    {
+        using var db = new AppDbContext();
+        return db.Contacts.ToList();
+    }
+
+    public static void UpdateContact(Contact contact)
+    {
+        using var db = new AppDbContext();
+        db.Contacts.Update(contact);
+        db.SaveChanges();
+    }
+
+    public static void DeleteContact(int id)
+    {
+        using var db = new AppDbContext();
+        var contact = db.Contacts.SingleOrDefault(x => x.Id == id);
+        db.Contacts.Remove(contact);
+        db.SaveChanges();
+    }
+}
---
+++ Kmakai.PhoneBook/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Kmakai.PhoneBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kmakai.PhoneBook.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.;Trusted_Connection=True;database=PhoneBook;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contact>().HasData(
+                       new Contact { Id = 1, Name = "John Doe", PhoneNumber = "1234567890", Email = "example@gmail.com" });
+    }
+}
